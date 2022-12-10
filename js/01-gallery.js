c88e520b922d5409516c2fac9dfcc6204cbfb5ac
Mariia Kovalova <mashuha1698@gmail.com>
2022-12-10 11:38:54 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryItems.forEach(({ preview, original, description }) => {
+  galleryEl.insertAdjacentHTML(
+    'beforeend',
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  );
+});
+
+galleryEl.addEventListener('click', onImgClick);
+
+function onImgClick(e) {
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+  e.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  galleryEl.addEventListener(
+    'keydown',
+    e => {
+      if (e.code === 'Escape') {
+        instance.close();
+      }
+    },
+    { once: true }
+  );
+}
