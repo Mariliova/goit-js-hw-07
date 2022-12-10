72f2c9e7693ea1708e2bdd50a8115b29bbc47e60
Mariia Kovalova <mashuha1698@gmail.com>
2022-12-10 12:51:18 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryItems.forEach(({ preview, original, description }) => {
+  galleryEl.insertAdjacentHTML(
+    'beforeend',
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  );
+});
+
+galleryEl.addEventListener('click', onImgClick);
+
+function onImgClick(e) {
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+  e.preventDefault();
+}
+
+// var lightbox = new SimpleLightbox('.gallery a', {
+//   /* options */
+// });
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+  // to do: show title
+});
+
+gallery.on('error.simplelightbox', function (e) {
+  console.log(e);
+});
